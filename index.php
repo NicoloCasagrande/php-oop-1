eb88dc7a30465e5cd9f655ca67d7d51ec8793d38
Nicolò Casagrande <nicolo.casagrande7@gmail.com>
2023-01-10 11:46:05 +0100

    implementazione db.php

--- index.php
+++ index.php
@@ -1,17 +1,5 @@
 <?php
-
-require_once __DIR__ . '/classes/Movie.php';
-require_once __DIR__ . '/classes/Category.php';
-
-$fantasy = new Category('fantasy');
-$horror = new Category('horror');
-$comedy = new Category('comedy');
-
-$movies = [
-    new Movie("l'esorcista", "Film di chissa chi chissa cosa", [$horror, $fantasy]),
-    new Movie("Scary Movie", "Film di chissa chi chissa cosa", [$comedy, $fantasy]),
-];
-
+require_once __DIR__ . '/db.php';
 ?>
 
 <!DOCTYPE html>
